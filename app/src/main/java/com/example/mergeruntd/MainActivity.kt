4ee6cf3478dfd10292d1e1b0f54a87e81f942786
nobuhiko-ryuu <138280739+nobuhiko-ryuu@users.noreply.github.com>
2026-02-21 13:02:27 +0900

    Fix ktlint import order in MainActivity

--- app/src/main/java/com/example/mergeruntd/MainActivity.kt
+++ app/src/main/java/com/example/mergeruntd/MainActivity.kt
@@ -17,8 +17,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.mergeruntd.ui.run.runScreen
